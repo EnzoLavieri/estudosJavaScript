a3842cbe71f5eddc53619614b5e52b281e362a67
EnzoLavieri <devenzolavieri@gmail.com>
2025-01-07 16:07:25 -0300

    variables types

--- 07-01/script.js
+++ 07-01/script.js
@@ -1,4 +1,13 @@
+const HW = "Hello World!";
+
 //console log aparece no console do navegador
-console.log("Hello World!!!");
+console.log(HW);
 //alert aparece na tela
-alert("Hello World!!!");
+alert(HW);
+
+/* 
+VAR | CONST | LET
+Use const por padrão.
+Se precisar mudar o valor da variavel, use let.
+Evite uso de var (ele pode causar bugs difíceis de encontrar).
+*/
