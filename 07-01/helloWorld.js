1987f0170eb25e43068bc9c6193b3210d5e98438
EnzoLavieri <devenzolavieri@gmail.com>
2025-01-08 14:05:40 -0300

    primitive data types bool, num, str e null

---
+++ 07-01/helloWorld.js
@@ -0,0 +1,4 @@
+//console log aparece no console do navegador
+console.log("Hello World!!!");
+//alert aparece na tela
+alert("Hello World!!!");
---
+++ 07-01/variableTypes.js
@@ -0,0 +1,12 @@
+/* 
+ CONST | LET | VAR 
+Use const por padrão.
+Se precisar mudar o valor da variavel, use let.
+Evite uso de var (ele pode causar bugs difíceis de encontrar).
+*/
+
+const HW = "Hello World!";
+//console log aparece no console do navegador
+console.log(HW);
+//alert aparece na tela
+alert(HW);
