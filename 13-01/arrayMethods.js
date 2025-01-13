e2eac88277b2c849d4d281715103d0c287a9a268
EnzoLavieri <devenzolavieri@gmail.com>
2025-01-13 15:33:54 -0300

    array interaction and transformation methods

--- 13-01/arrayMethods.js
+++ 13-01/arrayMethods.js
@@ -24,3 +24,37 @@ console.log(numeros);
 let frutas = ["maçã", "banana", "laranja"];
 frutas.splice(1, 1, "uva");
 console.log(frutas); // ["maçã", "uva", "laranja"]
+
+// ----- Métodos de Iteração e Transformação -----
+
+// forEach() – Itera sobre os elementos do array
+let nomes = ["Ana", "Bruno", "Carlos"];
+nomes.forEach((nome) => console.log(nome));
+
+//map() – Cria um novo array transformando cada elemento
+let numerosDobrados = [1, 2, 3].map((num) => num * 2);
+console.log(numerosDobrados);
+
+//filter() – Cria um novo array com elementos que passam no teste
+let numMaioresQueDois = [1, 2, 3, 4, 5].filter((num) => num > 2);
+console.log(numMaioresQueDois);
+
+//reduce() – Reduz o array a um único valor
+let soma = [1, 2, 3, 4, 5].reduce((acumulador, num) => acumulador + num, 0);
+console.log(soma);
+
+//find() – Retorna o primeiro elemento que atende a uma condição
+let primMaiorQueDois = [1, 2, 3, 4, 5].find((num) => num > 2);
+console.log(primMaiorQueDois);
+
+// findIndex() – Retorna o índice do primeiro elemento que atende a uma condição
+let indice = [1, 2, 3, 4, 5].findIndex((num) => num > 2);
+console.log(indice);
+
+//some() – Retorna true se pelo menos um elemento passar no teste
+let temMaiorQue3 = [1, 2, 3, 4].some((num) => num > 3);
+console.log(temMaiorQue3);
+
+//every() – Retorna true se todos os elementos passarem no teste
+let todosMaioresQue0 = [-1, 1, 2, 3].every((num) => num > 0);
+console.log(todosMaioresQue0); // true
