198de82092d6e4420428a3cb7d25e82aecf9c326
EnzoLavieri <devenzolavieri@gmail.com>
2025-01-13 14:39:42 -0300

    functions with callback

--- 13-01/functions.js
+++ 13-01/functions.js
@@ -7,7 +7,6 @@ saudacoes();
 function saudacoes() {
   console.log("saudacoes");
 }
-
 //--------------------------------------------------
 
 // Expressão de função (não sofre hoisting)
@@ -15,7 +14,6 @@ const saudacao = function () {
   console.log("Olá, mundo!");
 };
 saudacao();
-
 //--------------------------------------------------
 
 function soma(a, b) {
@@ -23,3 +21,44 @@ function soma(a, b) {
 }
 
 console.log(soma(3, 5)); // Saída: 8
+//--------------------------------------------------
+/*
+Função Anônima é uma função que não possui um nome explícito. Elas são frequentemente 
+usadas quando uma função não precisa ser referenciada em vários lugares do código, como em 
+callbacks ou atribuições diretas a variáveis.
+*/
+const saudacaoo = function (nome) {
+  return `Olá, ${nome}!`;
+};
+
+console.log(saudacaoo("Lucas"));
+
+/*
+  Com Arrow Functions, podemos reescrever a função anônima de forma mais concisa:
+*/
+const saudacaooo = (nome) => `Olá, ${nome}!`;
+
+console.log(saudacaooo("Lucas"));
+
+//--------------------------------------------------
+
+/*
+ **** Callback é uma função passada como argumento para outra função, para ser executada posteriormente.
+ */
+function processarEntrada(usuario, callback) {
+  console.log(`Processando usuário: ${usuario}`);
+  callback();
+}
+
+processarEntrada("Lucas", function () {
+  console.log("Usuário processado com sucesso!");
+});
+
+/*
+Callbacks com Arrow Functions
+*/
+function processarEntrada(usuario, callback) {
+  console.log(`Processando usuário: ${usuario}`);
+  callback();
+}
+processarEntrada("Joao", () => console.log("Usuário processado com sucesso!"));
