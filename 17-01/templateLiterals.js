6d4e5a169405644bbf9d36fa590124c689897ee5
EnzoLavieri <devenzolavieri@gmail.com>
2025-01-17 12:40:07 -0300

    expressoes integragas e tagged template

--- 17-01/templateLiterals.js
+++ 17-01/templateLiterals.js
@@ -17,12 +17,28 @@ console.log(mensagem2);
 //Vantagens: Mais legibilidade e menos concatenação com +
 
 //--Multi-line Strings
-// Traditional multi-line string
+//tradicional multi-line string
 const multiLine = "Esta é a primeira linha.\nEsta é a segunda linha.";
 
-// Using template literals
+//usando template literals
 const multiLine2 = `Esta é a primeira linha.
 Esta é a segunda linha.`;
 
 console.log(multiLine);
 console.log(multiLine2);
+
+//--Expressoes integradas
+const a = 5;
+const b = 10;
+const nomeEI = "João";
+console.log(`A soma de ${a} e ${b} é ${a + b}.`);
+
+//--Exemplo de Tagged Template
+function tag(strings, ...values) {
+  console.log(strings);
+  console.log(values);
+  return "Tagged template result";
+}
+
+const result = tag`Ola ${nomeEI}, voce tem ${a + b} novas mensagens.`;
+console.log(result);
