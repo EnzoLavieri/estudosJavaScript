3c8dbb353ee95d4bd58a34a036c9902f0eb86547
EnzoLavieri <devenzolavieri@gmail.com>
2025-01-17 10:55:37 -0300

    new file, template literals

---
+++ 17-01/templateLiterals.js
@@ -0,0 +1,17 @@
+/*
+Os Template Literals (ou Template Strings) são uma funcionalidade do JavaScript 
+introduzida no ES6 que permite criar strings de forma mais dinâmica e legível. 
+Em vez de usar aspas simples (' ') ou duplas (" "), utilizamos crases (` `). 
+*/
+
+//--Sintaxe Básica
+//com concatenacao:
+const nome = "João";
+const mensagem = "Olá, " + nome + "! Bem-vindo!";
+console.log(mensagem);
+
+//usando Template Literals
+const nome2 = "João";
+const mensagem2 = `Olá, ${nome2}! Bem-vindo!`;
+console.log(mensagem2);
+//Vantagens: Mais legibilidade e menos concatenação com +
