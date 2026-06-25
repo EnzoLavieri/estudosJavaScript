d2f5051d6b9dc731471f1304af817aef0327be66
EnzoLavieri <devenzolavieri@gmail.com>
2026-06-25 14:25:30 -0300

    exercicio 2 completo

---
+++ EXERCÍCIOS-Operadores/ex2.js
@@ -0,0 +1,14 @@
+/* 
+Crie um algoritmo que:
+1) solicite dois valores numéricos ao usuário; 
+2) divida os dois valores;
+3) retorne o valor final para o usuário.
+*/
+
+const val1 = 100
+const val2 = 50
+
+function divisorVal(val1, val2){
+    resultado = val1 / val2
+return resultado
+}
