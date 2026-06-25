f1031f0fb76bebacab15e3da65a854abb973a456
EnzoLavieri <devenzolavieri@gmail.com>
2026-06-25 15:04:23 -0300

    exercicio 3

---
+++ EXERCÍCIOS-Operadores/ex3.js
@@ -0,0 +1,11 @@
+/*
+Crie um algoritmo que:
+1) solicite a idade do usuário; 
+2) calcule quando dias o usuário viveu; 
+3) retorne o valor final para o usuário.
+*/
+
+const idade = 22
+const idadeEmDias = idade * 365  
+
+console.log(`Voce tem ${idade} anos e esta vivo á ${idadeEmDias} dias`)
